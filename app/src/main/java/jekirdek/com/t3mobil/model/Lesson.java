c29f582f20cc052db2799ed3ece080ce3a77655e
Cem Dırman <cemdrman@gmail.com>
2017-07-06 00:19:29 +0300

    added lesson model class

---
+++ app/src/main/java/jekirdek/com/t3mobil/model/Lesson.java
@@ -0,0 +1,26 @@
+package jekirdek.com.t3mobil.model;
+
+/**
+ * Created by cem on 6.07.2017.
+ */
+public class Lesson {
+
+    private int id;
+    private String lessonName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+}

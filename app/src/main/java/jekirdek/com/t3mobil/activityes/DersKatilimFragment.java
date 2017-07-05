13451ad8addf0b830ac36775eddda9507d476c49
Cem Dırman <cemdrman@gmail.com>
2017-07-06 01:08:05 +0300

    list of lesson json parsed

--- app/src/main/java/jekirdek/com/t3mobil/activityes/DersKatilimFragment.java
+++ app/src/main/java/jekirdek/com/t3mobil/activityes/DersKatilimFragment.java
@@ -6,14 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.List;
 
 import jekirdek.com.t3mobil.R;
+import jekirdek.com.t3mobil.utility.JsonParse;
+import jekirdek.com.t3mobil.utility.RequestURL;
 
 /**
  * Created by cem
  */
 public class DersKatilimFragment extends Fragment {
 
+    private Spinner cmbDers;
 
     @Nullable
     @Override
@@ -24,7 +37,39 @@ public class DersKatilimFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        cmbDers = (Spinner)view.findViewById(R.id.cmbDers);
+        getDersListesi();
+    }
 
+    private void getDersListesi(){
+
+        String loginRequestUrl = RequestURL.baseUrl.concat(RequestURL.dersListesiUrl);
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, loginRequestUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("response: " + response);
+                JsonParse jsonParse = new JsonParse();
+                List<String> lessons = jsonParse.getLessonList(response);
+
+                // Creating adapter for spinner
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, lessons);
+
+                // Drop down layout style - list view with radio button
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // attaching data adapter to spinner
+                cmbDers.setAdapter(dataAdapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("HATA OLUŞTU");
+            }
+        });
+        System.out.println("request:" + stringRequest.getUrl());
+        requestQueue.add(stringRequest);
     }
 
 }
